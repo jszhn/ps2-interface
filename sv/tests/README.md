e3c76522eaef1992f1b37d0e1c0db697960a39c3
Jason Zhang <zhangj1079@gmail.com>
2024-06-01 15:06:51 -0400

    clock generation testbench

--- sv/tests/README.md
+++ sv/tests/README.md
@@ -1,2 +1,4 @@
 ## Testbenches
 For this repository, testbenches are primarily written with [cocotb](https://github.com/cocotb/cocotb), a UNIX-based Python simulation library.
+
+This assumes Siemens ModelSim as the simulator. Please make sure it's path is in the system's environment variables.
--- sv/tests/read/test_ps2_read_data.py
+++ sv/tests/read/test_ps2_read_data.py
@@ -1,4 +1,22 @@
-# standard imports
+# cocotb imports
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
+import cocotb.handle as handle
+
+async def generate_ps2_clk (dut):
+    """
+    Drives the PS/2 clock line. Has a 15 ns start-up time before oscillations begin.
+    """
+    clk = dut.ps2_clock
+    clk.value = handle.Release()
+    clk.value = 1 # start-up before oscillations
+    await Timer (15, units="ns")
+
+    for cycle in range(12):
+        clk.value = 0
+        await Timer (15, units="ns")
+        clk.value = 1
+        await Timer (15, units="ns")
+    
+    clk.value = handle.Freeze()
