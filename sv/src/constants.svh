6dd7c7c847358a68acdf0b692061cbcde3c5d595
Jason Zhang <zhangj1079@gmail.com>
2024-06-03 13:36:43 -0400

    sv constants

---
+++ sv/src/constants.svh
@@ -0,0 +1,8 @@
+/*
+*   Helpful DE1-SoC-specific constants
+*           -- do not include globally! --
+*/
+
+parameter VGA_X_RES = 8'd160;
+parameter VGA_Y_RES = 8'd120;
+parameter CLOCK_FREQ = 13'd50000000; // FPGA frequency, 50 MHz
